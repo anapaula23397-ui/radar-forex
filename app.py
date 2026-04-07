b51d3fac1fc27cd5b2facaff66356cb44ffc0ac7
anapaula23397-ui <anapaula23397@gmail.com>
2026-04-07 16:35:08 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,20 @@
 from flask import Flask, jsonify, render_template
-import yfinance as yf
-import pandas as pd
 import os
+import random
 
 app = Flask(__name__)
 
 pares = [
-"EURUSD=X",
-"GBPUSD=X",
-"AUDUSD=X",
-"NZDUSD=X",
-"USDJPY=X",
-"USDCAD=X",
-"USDCHF=X",
-"EURJPY=X",
-"GBPJPY=X",
-"AUDJPY=X"
+"EURUSD",
+"GBPUSD",
+"USDJPY",
+"AUDUSD",
+"USDCAD",
+"USDCHF",
+"NZDUSD",
+"EURJPY",
+"GBPJPY",
+"AUDJPY"
 ]
 
 @app.route("/")
@@ -29,30 +28,15 @@ def sinais():
 
     for par in pares:
 
-        try:
-
-            data = yf.download(par, interval="1m", period="1d")
-
-            close = data["Close"]
-
-            ema9 = close.ewm(span=9).mean()
-            ema21 = close.ewm(span=21).mean()
-
-            if ema9.iloc[-1] > ema21.iloc[-1]:
-                sinal = "COMPRA"
-            else:
-                sinal = "VENDA"
-
-        except:
-            sinal = "SEM DADOS"
+        sinal = random.choice(["COMPRA","VENDA"])
 
         resultados.append({
-            "par": par,
-            "sinal": sinal
+            "par":par,
+            "sinal":sinal
         })
 
     return jsonify(resultados)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT",3000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0",port=port)
